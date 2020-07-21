d76f07c3efcae8c656e116ddec7db428d56a4f3d
Leonel Ramos <lramos.terp@gmail.com>
2020-07-21 13:23:32 -0400

    added routing to book renderer

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,22 @@
 import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 import React from "react";
+import { Router } from "@reach/router";
 import Toolbar from "./Toolbar";
 import Bookshelf from "./Bookshelf";
+import BookRenderer from "../components/FileRender";
 import "./app.scss";
 
 const App = () => {
   return (
     <ThemeProvider>
       <CSSReset />
-      <Toolbar />
-      <Bookshelf />
+      <Router>
+        <Toolbar path="/" />
+      </Router>
+      <Router>
+        <Bookshelf path="/" />
+        <BookRenderer path="book/:id/" />
+      </Router>
     </ThemeProvider>
   );
 };
