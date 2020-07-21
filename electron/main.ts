fe6ff34c656e2feb9b9489ef0d03515f9ad23816
Leonel Ramos <lramos.terp@gmail.com>
2020-07-21 06:48:47 -0400

    removed window frame

--- electron/main.ts
+++ electron/main.ts
@@ -1,6 +1,6 @@
-import { app, BrowserWindow } from 'electron';
-import * as path from 'path';
-import * as url from 'url';
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import url from "url";
 
 let mainWindow: Electron.BrowserWindow | null;
 
@@ -8,28 +8,27 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    frame: process.env.NODE_ENV === 'development',
+    frame: false,
     webPreferences: {
       nodeIntegration: true,
     },
   });
 
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === "development") {
     mainWindow.loadURL(`http://localhost:4000`);
   } else {
     mainWindow.loadURL(
       url.format({
-          pathname: path.join(__dirname, '../src/index.html'),
-          protocol: 'file:',
-          slashes: true
+        pathname: path.join(__dirname, "../src/index.html"),
+        protocol: "file:",
+        slashes: true,
       })
     );
   }
-  
-  mainWindow.on('closed', () => {
+
+  mainWindow.on("closed", () => {
     mainWindow = null;
   });
 }
 
-app.on('ready', createWindow);
-app.allowRendererProcessReuse = true;
+app.on("ready", createWindow);
